9ec12e5104f46e6b9f787d201e53465b038684a6
Vincent Samuel <chintuvincent@gmail.com>
2024-07-01 13:04:31 +0530

    institucional mobile comlpeted

--- src/Components/navbar/Sidebar.jsx
+++ src/Components/navbar/Sidebar.jsx
@@ -1,19 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import {Link as Scroll} from "react-scroll"
 import "./Navbar.css";
 
 const Sidebar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
   };
+
+  const toggleDropdown = () => {
+    if (isDropdownOpen) {
+      setMenuOpen(false); // Close the menu if the dropdown is already open
+    }
+    setDropdownOpen(!isDropdownOpen);
+  };
+
 
   useEffect(() => {
     if (isMenuOpen) {
       document.body.classList.add("no-scroll");
     } else {
       document.body.classList.remove("no-scroll");
+      setDropdownOpen(false);
     }
   }, [isMenuOpen]);
 
@@ -34,7 +45,31 @@ const Sidebar = () => {
           </Link>
         </div>
         <div className="menuList">
-          <p>Institucional</p>
+          <Link to="/institucional" onClick={toggleDropdown}>
+            <p>Institucional</p>
+          </Link>
+          {isDropdownOpen && (
+            <div className="sub-menu">
+              <Scroll smooth={true} duration={500} onClick={toggleMenu} to="desarrollar">
+                <p>Propósito y Actividades</p>
+              </Scroll>
+              <div className="whiteLine" />
+              <Scroll smooth={true} duration={500} onClick={toggleMenu} to="fundadoras">
+                <p>Instituciones fundadoras</p>
+              </Scroll>
+              <div className="whiteLine" />
+
+              <Scroll smooth={true} duration={500} onClick={toggleMenu} to="fundacionales">
+                <p>Antecedentes fundacionales</p>
+              </Scroll>
+              <div className="whiteLine" />
+              <Scroll smooth={true} duration={500} onClick={toggleMenu} to="redAcademica">
+                <p>Red Académica</p>
+              </Scroll>
+              <div className="whiteLine" />
+              <br></br>
+            </div>
+          )}
           <div className="mRedLine" />
           <Link to="/comunidadAcademica" onClick={toggleMenu}>
             <p>Comunidad academica</p>
